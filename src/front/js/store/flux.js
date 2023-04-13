7c0e3a34f3eeadabedd27c775f0bb3b217a0b647
metantonio <antonio_martinez88@hotmail.com>
2023-04-13 00:59:27 +0000

    updated

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -22,14 +22,15 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
+					const store = getStore()
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
-					setStore({ message: data.message })
+					setStore({ ...store, message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
